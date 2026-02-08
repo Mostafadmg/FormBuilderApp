4bedceec82382eca04397a062f61249a86a52089
Mostafadmg <mostafa.damghani.md@gmail.com>
2026-02-08 21:17:43 +0000

    html for step 1 and 2 added

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Main application entry point
+import { App } from '../js/components/app';
+
+const root = document.getElementById('app');
+
+/* root.innerHTML = App();
+ */
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
+  plugins: [tailwindcss()],
   server: {
-    open: true
-  }
-})
+    open: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
